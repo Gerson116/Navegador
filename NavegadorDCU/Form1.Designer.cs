6c1754c03c51bd41a6cfbdb61d96fa7e827a5639
Gerson116 <gerson.santosm3@gmail.com>
2017-06-05 09:28:43 -0400

    Este es un navegador desarrollado en C#
    Este navegador fue diseñado para la asignatura de Diseño centrado en el usuario(DCU).

---
+++ NavegadorDCU/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace NavegadorDCU
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cbxHistorial = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnHistory = new System.Windows.Forms.Button();
+            this.cbxFavorito = new System.Windows.Forms.ComboBox();
+            this.btnFV = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.cbxSearch = new System.Windows.Forms.ComboBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cbxHistorial);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1571, 53);
+            this.panel1.TabIndex = 0;
+            // 
+            // cbxHistorial
+            // 
+            this.cbxHistorial.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbxHistorial.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxHistorial.FormattingEnabled = true;
+            this.cbxHistorial.Location = new System.Drawing.Point(1335, 3);
+            this.cbxHistorial.Name = "cbxHistorial";
+            this.cbxHistorial.Size = new System.Drawing.Size(233, 33);
+            this.cbxHistorial.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(256, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(247, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Eliminar ventana";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(247, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Nueva ventana";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tabControl1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 53);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.676742F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 92.32326F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1571, 683);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 8;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel2.Controls.Add(this.btnHistory, 7, 0);
+            this.tableLayoutPanel2.Controls.Add(this.cbxFavorito, 6, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnFV, 5, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnRefresh, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnForward, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnBack, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnHome, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.cbxSearch, 4, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1565, 46);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // btnHistory
+            // 
+            this.btnHistory.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnHistory.Location = new System.Drawing.Point(1488, 3);
+            this.btnHistory.Name = "btnHistory";
+            this.btnHistory.Size = new System.Drawing.Size(74, 40);
+            this.btnHistory.TabIndex = 7;
+            this.btnHistory.Text = "Historial";
+            this.btnHistory.UseVisualStyleBackColor = true;
+            this.btnHistory.Click += new System.EventHandler(this.btnHistory_Click);
+            // 
+            // cbxFavorito
+            // 
+            this.cbxFavorito.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cbxFavorito.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxFavorito.FormattingEnabled = true;
+            this.cbxFavorito.Location = new System.Drawing.Point(1332, 3);
+            this.cbxFavorito.Name = "cbxFavorito";
+            this.cbxFavorito.Size = new System.Drawing.Size(150, 33);
+            this.cbxFavorito.TabIndex = 6;
+            this.cbxFavorito.SelectedIndexChanged += new System.EventHandler(this.cbxFavorito_SelectedIndexChanged);
+            // 
+            // btnFV
+            // 
+            this.btnFV.BackgroundImage = global::NavegadorDCU.Properties.Resources.estrella_de_mario;
+            this.btnFV.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnFV.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnFV.FlatAppearance.BorderSize = 0;
+            this.btnFV.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFV.Location = new System.Drawing.Point(1254, 3);
+            this.btnFV.Name = "btnFV";
+            this.btnFV.Size = new System.Drawing.Size(72, 40);
+            this.btnFV.TabIndex = 5;
+            this.btnFV.UseVisualStyleBackColor = true;
+            this.btnFV.Click += new System.EventHandler(this.btnFV_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.BackgroundImage = global::NavegadorDCU.Properties.Resources._29209_200;
+            this.btnRefresh.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRefresh.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnRefresh.FlatAppearance.BorderSize = 0;
+            this.btnRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRefresh.Location = new System.Drawing.Point(237, 3);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(72, 40);
+            this.btnRefresh.TabIndex = 3;
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.BackgroundImage = global::NavegadorDCU.Properties.Resources.thXI04Q2EC;
+            this.btnForward.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnForward.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnForward.FlatAppearance.BorderSize = 0;
+            this.btnForward.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnForward.Location = new System.Drawing.Point(159, 3);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(72, 40);
+            this.btnForward.TabIndex = 2;
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackgroundImage = global::NavegadorDCU.Properties.Resources._17264;
+            this.btnBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnBack.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnBack.FlatAppearance.BorderSize = 0;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBack.Location = new System.Drawing.Point(81, 3);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(72, 40);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnHome
+            // 
+            this.btnHome.BackgroundImage = global::NavegadorDCU.Properties.Resources.logo_home;
+            this.btnHome.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnHome.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnHome.FlatAppearance.BorderSize = 0;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHome.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnHome.Location = new System.Drawing.Point(3, 3);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(72, 40);
+            this.btnHome.TabIndex = 0;
+            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // cbxSearch
+            // 
+            this.cbxSearch.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cbxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxSearch.FormattingEnabled = true;
+            this.cbxSearch.Location = new System.Drawing.Point(315, 3);
+            this.cbxSearch.Name = "cbxSearch";
+            this.cbxSearch.Size = new System.Drawing.Size(933, 33);
+            this.cbxSearch.TabIndex = 4;
+            this.cbxSearch.SelectedIndexChanged += new System.EventHandler(this.cbxSearch_SelectedIndexChanged);
+            this.cbxSearch.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cbxSearch_KeyPress);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(3, 55);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1565, 625);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.webBrowser1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1557, 596);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(3, 3);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1551, 590);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1571, 736);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Navegadr Zero";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button btnHistory;
+        private System.Windows.Forms.ComboBox cbxFavorito;
+        private System.Windows.Forms.Button btnFV;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.ComboBox cbxSearch;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ComboBox cbxHistorial;
+    }
+}
+
---
+++ NavegadorDCU/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NavegadorDCU
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            cbxHistorial.Hide();
+
+        }
+
+        List<string> busquedas = new List<string>();
+        List<string> favoritos = new List<string>();
+        List<string> Historial = new List<string>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.bing.com/");
+            webBrowser1.DocumentCompleted += WebBrowser_DocumentCompleted;
+            
+        }
+
+        private void WebBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            tabControl1.SelectedTab.Text = webBrowser1.DocumentTitle;
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            WebBrowser web = tabControl1.SelectedTab.Controls[0] as WebBrowser;
+            if (web != null)
+            {
+                web.Navigate("https://www.bing.com/");
+            }
+
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            WebBrowser web = tabControl1.SelectedTab.Controls[0] as WebBrowser;
+            if (web != null) {
+
+                if (web.CanGoBack) {
+                    web.GoBack();
+                }
+            }
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            WebBrowser web = tabControl1.SelectedTab.Controls[0] as WebBrowser;
+            if (web != null) {
+
+                if (web.CanGoForward) {
+                    web.GoForward();
+                }
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            WebBrowser web = tabControl1.SelectedTab.Controls[0] as WebBrowser;
+            if (web != null)
+            {
+                web.Refresh();
+            }
+        }
+
+        private void cbxSearch_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFV_Click(object sender, EventArgs e)
+        {
+            favoritos.Add(webBrowser1.Url.ToString());
+            actualizarFavoritos();
+        }
+
+        private void actualizarFavoritos() {
+            cbxFavorito.Items.Clear();
+
+            foreach (string addFavo in favoritos) {
+                cbxFavorito.Items.Add(addFavo);
+
+            }
+            
+
+        }
+
+        private void cbxSearch_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+            if (e.KeyChar == Convert.ToChar(Keys.Enter)) {
+
+                WebBrowser web = tabControl1.SelectedTab.Controls[0] as WebBrowser;
+                if (web != null)
+                {
+                    web.Navigate(cbxSearch.Text);
+
+                    busquedas.Add(web.Url.ToString());
+                    actualizarBarraBusqueda();
+
+                    if (web != null)
+                    {
+                        web.Navigate(cbxSearch.Text);
+                        Historial.Add(web.Url.ToString());
+                        actualizarNavegador();
+                    }
+                }
+            }
+   
+        }
+
+        private void actualizarBarraBusqueda()
+        {
+            cbxSearch.Items.Clear();
+
+            foreach (string busquedasRealizadas in busquedas) {
+                cbxSearch.Items.Add(busquedasRealizadas);
+            }
+        }
+
+        private void actualizarNavegador() {
+            cbxHistorial.Items.Clear();
+            foreach (string historialNavegador in Historial) {
+                cbxHistorial.Items.Add(historialNavegador);
+            }
+
+        }
+
+        private void cbxFavorito_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        WebBrowser webTab = null;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TabPage tab = new TabPage();
+            tab.Text = "Nueva ventana";
+            tabControl1.Controls.Add(tab);
+            tabControl1.SelectTab(tabControl1.TabCount - 1);
+            webTab = new WebBrowser() { ScriptErrorsSuppressed = true };
+            webTab.Parent = tab;
+            webTab.Dock = DockStyle.Fill;
+            webTab.Navigate("https://www.bing.com/");
+            cbxSearch.Text = "https://www.bing.com/";
+            webTab.DocumentCompleted += webTab_DocumentCompleted;
+        }
+
+        private void webTab_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            tabControl1.SelectedTab.Text = webTab.DocumentTitle;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            tabControl1.TabPages.RemoveAt(tabControl1.SelectedIndex);
+                
+        }
+
+        private void btnHistory_Click(object sender, EventArgs e)
+        {
+            cbxHistorial.Show();
+            
+             
+        }
+    }
+}
